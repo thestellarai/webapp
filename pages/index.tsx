c45806faf0db66620e39592229793a87347242c7
ChaseRensberger <chasehrensberger@gmail.com>
2022-07-10 01:03:58 -0400

    typescript and tailwind

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className='flex flex-col justify-center items-center w-screen h-screen'>
+      <div className="text-3xl font-bold">
+        StellarJobs
+      </div>
+    </div>
+    
+  )
+}
